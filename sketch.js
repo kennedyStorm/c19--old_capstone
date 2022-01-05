6e7fc2d06af427e1f2b77d535a027df38800ad0e
kennedyStorm <93315034+kennedyStorm@users.noreply.github.com>
2022-01-05 08:53:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,234 @@
+var msg = "This is a global message";
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
+
+var girl, girl_running, zombie_girl_collided;
+var ground, invisibleGround, groundImage;
+
+var cloudsGroup, cloudImage;
+var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
+
+var score;
+var gameOverImg,restartImg
+var jumpSound , checkPointSound, dieSound
+
+function preload(){
+  var msg = "Preload";
+  console.log(msg);
+
+  girl_running = loadAnimation("cute_girl1.png","cute_girl22.png");
+  zombie_girl_collided = loadAnimation("zombie_girl.png");
+  
+  groundImage = loadImage("ground2.png");
+  
+  cloudImage = loadImage("cloud.png");
+  
+  obstacle1 = loadImage("obstacle1.png");
+  obstacle2 = loadImage("obstacle2.png");
+  obstacle3 = loadImage("obstacle3.png");
+  obstacle4 = loadImage("obstacle4.png");
+  obstacle5 = loadImage("obstacle5.png");
+  obstacle6 = loadImage("obstacle6.png");
+  
+  restartImg = loadImage("restart.png")
+  gameOverImg = loadImage("gameOver.png")
+  
+  jumpSound = loadSound("jump.mp3")
+  dieSound = loadSound("die.mp3")
+  checkPointSound = loadSound("checkPoint.mp3")
+}
+
+function setup() {
+  // var msg = "Message in set up";
+  console.log(msg);
+  createCanvas(windowWidth, windowHeight);
+
+//   var message = "This is a message";
+//  console.log(message)
+  
+  girl = createSprite(50,160,20,50);
+  girl.addAnimation("running", girl_running);
+  girl.addAnimation("collided", zombie_girl_collided);
+  
+
+  trex.scale = 0.5;
+  
+  ground = createSprite(width/2,height,width,2);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  
+  gameOver = createSprite(300,100);
+  gameOver.addImage(gameOverImg);
+  
+  restart = createSprite(300,140);
+  restart.addImage(restartImg);
+  
+ 
+  gameOver.scale = 0.5;
+  restart.scale = 0.5;
+  
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  
+  //create Obstacle and Cloud Groups
+  obstaclesGroup = createGroup();
+  cloudsGroup = createGroup();
+
+  
+  trex.setCollider("rectangle",0,0,trex.width,trex.height);
+  trex.debug = true
+  
+  score = 0;
+  
+}
+
+function draw() {
+  
+  background(180);
+  //displaying score
+  text("Score: "+ score, 500,50);
+  
+  
+  if(gameState === PLAY){
+
+    gameOver.visible = false;
+    restart.visible = false;
+    
+
+    ground.velocityX = -(4 + 3* score/100)
+    //scoring
+    score = score + Math.round(getFrameRate()/60);
+    
+    if(score>0 && score%100 === 0){
+       checkPointSound.play() 
+    }
+    
+    if (ground.x < 0){
+      ground.x = ground.width/2;
+    }
+    
+    //jump when the space key is pressed
+    if((touches.length>0)||(keyDown("space")&& girl.y >= 100)) {
+        girl.velocityY = -12;
+        jumpSound.play();
+        touches = [];
+    }
+    
+    //add gravity
+    girl.velocityY = girl.velocityY + 0.8
+  
+    //spawn the clouds
+    spawnClouds();
+  
+    //spawn obstacles on the ground
+    spawnObstacles();
+    
+    if(obstaclesGroup.isTouching(girl)){
+        //trex.velocityY = -12;
+        jumpSound.play();
+        gameState = END;
+        dieSound.play()
+      
+    }
+  }
+   else if (gameState === END) {
+      gameOver.visible = true;
+      restart.visible = true;
+      if(mousePressedOver(restart)){
+          reset();
+
+      }
+     //change the girl animation
+      girl.changeAnimation("collided", zombie_girl_collided);
+    
+     
+     
+      ground.velocityX = 0;
+      girl.velocityY = 0
+      
+     
+      //set lifetime of the game objects so that they are never destroyed
+    obstaclesGroup.setLifetimeEach(-1);
+    cloudsGroup.setLifetimeEach(-1);
+     
+     obstaclesGroup.setVelocityXEach(0);
+     cloudsGroup.setVelocityXEach(0);    
+   }
+  
+ 
+  //stop trex from falling down
+  girl.collide(invisibleGround);
+  
+  
+
+
+  drawSprites();
+}
+
+function reset(){
+  gameState=PLAY;
+  gameOver.visible=false;
+  restart.visible = false;
+  obstaclesGroup.destroyEach();
+  cloudsGroup.destroyEach();
+  score = 0;
+  girl.changeAnimation("running", girl_running);
+  
+
+}
+
+
+function spawnObstacles(){
+ if (frameCount % 60 === 0){
+   var obstacle = createSprite(600,165,10,40);
+   obstacle.velocityX = -(6 + score/100);
+   
+    //generate random obstacles
+    var rand = Math.round(random(1,6));
+    switch(rand) {
+      case 1: obstacle.addImage(obstacle1);
+              break;
+      case 2: obstacle.addImage(obstacle2);
+              break;
+      case 3: obstacle.addImage(obstacle3);
+              break;
+      case 4: obstacle.addImage(obstacle4);
+              break;
+      case 5: obstacle.addImage(obstacle5);
+              break;
+      case 6: obstacle.addImage(obstacle6);
+              break;
+      default: break;
+    }
+   
+    //assign scale and lifetime to the obstacle           
+    obstacle.scale = 0.5;
+    obstacle.lifetime = 300;
+   
+   //add each obstacle to the group
+    obstaclesGroup.add(obstacle);
+ }
+}
+
+function spawnClouds() {
+  //write code here to spawn the clouds
+  if (frameCount % 60 === 0) {
+    var cloud = createSprite(600,120,40,10);
+    cloud.y = Math.round(random(80,120));
+    cloud.addImage(cloudImage);
+    cloud.scale = 0.5;
+    cloud.velocityX = -3;
+    
+     //assign lifetime to the variable
+    cloud.lifetime = 200;
+    
+    //adjust the depth
+    cloud.depth = trex.depth;
+    trex.depth = trex.depth + 1;
+    
+    //add each cloud to the group
+    cloudsGroup.add(cloud);
+  }
+}
+
